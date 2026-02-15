36af39da68f129347b8addbd8c642cb6838bc246
Козак Мар'ян <marian.kozak.pp.2023@lpnu.ua>
2026-02-15 17:06:58 +0200

    feat(layout): add initial layout components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "16px", borderBottom: "1px solid #333" }}>
+      <h1>Lab 1 – React + GitFlow</h1>
+    </header>
+  );
+}
