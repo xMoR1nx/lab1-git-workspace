5d784eff628ce6c6021f1c79b98b925e8a0a2ff7
Козак Мар'ян <marian.kozak.pp.2023@lpnu.ua>
2026-03-15 17:07:12 +0200

    feat: add Sentry error tracking and monitoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
+import * as Sentry from '@sentry/react'
+
 export default function Header() {
   const appStatus = import.meta.env.VITE_APP_STATUS || 'Development'
   const isProduction = import.meta.env.PROD
+
+  function throwError() {
+  try {
+    throw new Error(`Sentry Test Error #${Date.now()}`)
+  } catch (error) {
+    Sentry.captureException(error)
+    console.error("Error sent to Sentry:", error)
+  }
+}
 
   return (
     <header className="app-header">
@@ -15,6 +26,12 @@ export default function Header() {
         <span className={`env-badge ${isProduction ? 'badge-prod' : 'badge-dev'}`}>
           {appStatus}
         </span>
+        <button
+          onClick={throwError}
+          style={{ background: 'red', color: 'white', border: 'none', padding: '6px 12px', borderRadius: '6px', cursor: 'pointer', fontSize: '13px' }}
+        >
+          💥 Break the world
+        </button>
       </div>
     </header>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './styles/app.css'
 import App from './App.jsx'
 import { PostHogProvider } from '@posthog/react'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  environment: "development",
+  sendDefaultPii: true,
+})
+
+// Встановлюю контекст користувача
+Sentry.setUser({
+  id: "12345",
+  email: "marian.kozak.pp.2023@lpnu.ua",
+  segment: "student_user",
+})
 
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
